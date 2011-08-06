f79fbd6b6ea10044c2709b5092a89d3d094060b7
Hanging Chad Ogea <hco-github@digamma.net>
2011-08-06 19:23:25 -0400

    1. Display the name of the post author when you're ignoring it. 2. Clean up the code. Put the ugly HTML stuff into its own function. Eliminate the unused trim function.

--- content_script.js
+++ content_script.js
@@ -1,5 +1,4 @@
 function whyIgnore(element, kill_regex) {
-//  return (element.id == "c9");
   if (element.id.substr(-1) != "9") {
     return false; // This is not a post row. They all have IDs ending in 9.
   }
@@ -9,14 +8,7 @@ function whyIgnore(element, kill_regex) {
   return element.innerHTML.match(kill_regex);
 }
 
-function trim(str) {
-  var newstr;
-  newstr = str.replace(/^\s*/, "").replace(/\s*$/, ""); 
-  newstr = newstr.replace(/\s{2,}/, " "); 
-  return newstr;
-} 
-
-function after_send_request(response){
+function actually_do_things(response){
   var kill_regex = response.kill_regex;
   if (kill_regex == undefined)
     kill_regex = "quote_author..(HAL McRae|Yun Taragoashi)";
@@ -25,22 +17,35 @@ function after_send_request(response){
   for(var i=0;i<el.length;i++){
     var reason = whyIgnore(el[i], kill_regex);
     if (reason) {
-      el[i].style.display = "none";
-      var ignoreBarElement = document.createElement('tr');
-      var firstCell = document.createElement('td');
-      firstCell.setAttribute("colspan", "3");
-      firstTable = document.createElement('table');
-      firstTable.setAttribute("class", "ignored");
-      
-      firstTable.setAttribute("style", "width:100%");
-      firstTable.innerHTML = "<tr><td class=\"tableCellOne\"><span style=\"float:right\"><a href=\"#\" onclick=\"showHideRow(\'" + el[i].id + "\');return false;\">View / Hide</a></span>A post is hidden because it contains the string \'" + reason[0] + "\'.</td></tr>"
-      firstCell.appendChild(firstTable);
-      ignoreBarElement.appendChild(firstCell);
-      elementParent = el[i].parentNode;
-      elementParent.insertBefore(ignoreBarElement, el[i]);
+      set_post_ignored(el[i], reason);
       i = i+2;
     }
   }
 }
 
-chrome.extension.sendRequest({method: "get_kill_regex"}, after_send_request);
+function set_post_ignored(post_row, reason){
+  post_row.style.display = "none";
+  var ignoreBarElement = document.createElement('tr');
+  var firstCell = document.createElement('td');
+  firstCell.setAttribute("colspan", "3");
+  firstTable = document.createElement('table');
+  firstTable.setAttribute("class", "ignored");
+  firstTable.setAttribute("style", "width:100%");
+  firstTable.innerHTML = "<tr><td class=\"tableCellOne\"><span style=\"float:" +
+      "right\"><a href=\"#\" onclick=\"showHideRow(\'" + post_row.id + "\');" +
+      "return false;\">View / Hide</a></span>A post by <b>" + 
+      comment_author(post_row) + "</b> is hidden because it contains the " +
+      "string \'" + reason[0] + "\'.</td></tr>";
+  firstCell.appendChild(firstTable);
+  ignoreBarElement.appendChild(firstCell);
+  elementParent = post_row.parentNode;
+  elementParent.insertBefore(ignoreBarElement, post_row);
+}
+
+function comment_author(comment_row) {
+  var anchors = comment_row.getElementsByTagName("a");
+  var author_link = anchors[0];
+  return author_link.innerText;
+}
+
+chrome.extension.sendRequest({method: "get_kill_regex"}, actually_do_things);
